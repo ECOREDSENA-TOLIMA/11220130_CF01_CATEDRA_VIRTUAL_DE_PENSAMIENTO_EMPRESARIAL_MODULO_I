d7dce01bd34ce80bcd77b0710b23a5dc0e69201e
veimarcm <vcelis@sena.edu.co>
2024-09-03 09:51:35 -0500

    Video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -15,7 +15,7 @@
         p Esta afirmación, es quizás las que más poder tiene en la vida de las personas que desean emprender y seguir su proyecto de vida. Pero más que una afirmación, es un proceso que deben tener las personas, así que para descifrar lo que ello conlleva, se invita a que inicie este componente formativo, analizando el siguiente video introductorio a la temática que se explicará:
     figure.mb-5(data-aos="zoom-in")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="1280" height="720" src="https://www.youtube.com/embed/FivCJk9spzE" title="Pensamiento estratégico y creación de empresa" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen)
 
     
 </template>
