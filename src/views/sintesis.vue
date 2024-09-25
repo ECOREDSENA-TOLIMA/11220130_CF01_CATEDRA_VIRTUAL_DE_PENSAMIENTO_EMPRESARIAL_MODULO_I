ba7b7fec2f1a0b0d0062580db72338b79763982f
veimarcm <vcelis@sena.edu.co>
2024-09-25 09:58:50 -0500

    CF01FINAL

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -2,13 +2,11 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-
     p.mb-5(data-aos="fade-right") A continuación, se describe a través de un mapa conceptual la temática tratada durante este componente formativo, el cual destaca los aspectos más relevantes que hacen parte del pensamiento estratégico y la creación de empresa.
-
     .row.justify-content-center
       .col-lg-10.mb-5(data-aos="fade-right")
         figure
-          img(src="@/assets/curso/temas/sintesis.svg", alt="Imagen que contiene el mapa conceptual que resume la temática tratada durante este componente formativo, partiendo del hecho que la empresa tiene presente el pensamiento estratégico que tiene como fin el identificar las acciones del entorno; además, resalta que, para crear empresa, se debe analizar el entorno y crear un plan estratégico que esté basado en el proyecto de vida. ")
+          img(src="@/assets/curso/temas/sintesis.svg", alt="Imagen que contiene el mapa conceptual que resume la temática tratada durante este componente formativo, partiendo del hecho que la empresa tiene presente el pensamiento estratégico que tiene como fin el identificar las acciones del entorno; además, resalta que, para crear empresa, se debe analizar el entorno y crear un plan estratégico que esté basado en el proyecto de vida.")
       .col-auto(data-aos="fade-left")
         a.anexo.mb-4(:href="obtenerLink('/downloads/sintesis.pdf')" target="_blank")
           .anexo__icono
